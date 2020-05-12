d895ddfaf67b6c6954ae59445333f16e6a5ea5c7
ahmed <ahmedsh717@gmail.com>
2020-05-12 16:08:02 +0300

    msg

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # about-me
+
+## lab-02 :
+is about asking the user 5 quastions and use their answers in ther process
+
+## lab-03 :
+is about compliting the guissing game by adding another quastion
--- js/app.js
+++ js/app.js
@@ -1,54 +1,173 @@
-'use strict'
-
+'use strict' 
 var wrong = 0;
 var right = 0;
-function Quastion(mystring) {
-    var Q = prompt(mystring);
-    console.log(Q);
-    if (Q.toLowerCase() == 'y' || Q.toLowerCase() == 'yes') {
-        console.log(right);
-        right = right + 1;
-        return true;
-    } else {
-        console.log(wrong);
-        wrong = wrong + 1;
-        return false;
-    }
-}
+// function Quastion(mystring) {
+//     var Q = prompt(mystring);
+//     console.log(Q);
+//     if (Q.toLowerCase() == 'y' || Q.toLowerCase() == 'yes') {
+//         // console.log(right);
+//         // right = right + 1;
+//         return true;
+//     } else {
+//         // console.log(wrong);
+//         // wrong = wrong + 1;
+//         return false;
+//     }
+// }
 
 var UserName = prompt("What is your name?");
 alert(`Wellcome to my Website ${UserName}`);
 
-if (Quastion("Do you think that I Like Coffee ??") == true) {
+var Q1 = prompt("Do you think that I Like Coffee ??");
+console.log(Q1);
+if (Q1.toLowerCase() == 'y' || Q1.toLowerCase() == 'yes') {
     alert(`Yeah ${UserName} that is right I love Coffee`)
+    right = right + 1;
 } else {
     alert(`Sorry ${UserName} That is Wrong`)
+    wrong = wrong + 1;
 }
-/********************************************************** */
-if (Quastion("Do you think that I Watched GOT ??") == true) {
-    alert(`No ${UserName} I Didn't and I wont`)
-} else {
-    alert(`Ofcourse I didn't ${UserName} That is right`)
-}
-/********************************************************** */
-if (Quastion("Do you think that I whatched 'Lacasa De Papel' ??") == true) {
+// /********************************************************** */
+var Q2 = prompt("Do you think that I whatched 'Lacasa De Papel' ??");
+console.log(Q2);
+if (Q2.toLowerCase() == 'y' || Q2.toLowerCase() == 'yes') {
     alert(`Yeah ${UserName} that is right`)
+    right = right + 1;
 } else {
     alert(`Sorry ${UserName} That is Wrong`)
+    wrong = wrong + 1;
 }
-/********************************************************** */
-if (Quastion("Do you think that I can solve Robik's Cube in 2 Minites ??") == true) {
-    alert(`Yeah ${UserName} that is right`)
+// /********************************************************** */
+var Q3 = prompt("Do you think that I can solve Robik's Cube in 2 Minites ??");
+console.log(Q3);
+if (Q3.toLowerCase() == 'y' || Q3.toLowerCase() == 'yes') {
+    alert(`Yeah ${UserName} that is right `)
+    right = right + 1;
 } else {
     alert(`Sorry ${UserName} That is Wrong`)
+    wrong = wrong + 1;
 }
-/********************************************************** */
-if (Quastion("Do you think that I Watch animations stuff ??") == true) {
+// /********************************************************** */
+var Q4 = prompt("Do you think that I Watch animations stuff ??");
+console.log(Q4);
+if (Q4.toLowerCase() == 'y' || Q4.toLowerCase() == 'yes') {
     alert(`No ${UserName} grow up it's for kids`)
+    wrong = wrong + 1;
 } else {
     alert(`Yeah ${UserName} That is right`)
+    right = right + 1;
 }
+// /********************************************************** */
+var Q5 = prompt("Do you think that I Watched GOT ??");
+console.log(Q5);
+if (Q5.toLowerCase() == 'y' || Q5.toLowerCase() == 'yes') {
+    alert(`No ${UserName} I Didn't and I wont`)
+    wrong = wrong + 1;
+} else {
+    alert(`Ofcourse I didn't ${UserName} That is right`)
+    right = right + 1;
+}
+// /********************************************************** */
+// if (Quastion("") == true) {
+//     alert(`No ${UserName} I Didn't and I wont`)
+//     wrong = wrong + 1;
+// } else {
+//     alert(`Ofcourse I didn't ${UserName} That is right`)
+//     right = right + 1;
+// }
+// /********************************************************** */
+// if (Quastion("") == true) {
+//     alert(`Yeah ${UserName} that is right`)
+//     right = right + 1;
+// } else {
+//     alert(`Sorry ${UserName} That is Wrong`)
+//     wrong = wrong + 1;
+// }
+// /********************************************************** */
+// if (Quastion("") == true) {
+//     alert(`Yeah ${UserName} that is right`)
+//     right = right + 1;
+// } else {
+//     alert(`Sorry ${UserName} That is Wrong`)
+//     wrong = wrong + 1;
+// }
+// /********************************************************** */
+// if (Quastion("") == true) {
+//     alert(`No ${UserName} grow up it's for kids`)
+//     wrong = wrong + 1;
+// } else {
+//     alert(`Yeah ${UserName} That is right`)
+//     right = right + 1;
+// }
 /********************************************************** */
+var attempt = 0;
+do {
+    attempt = attempt + 1;
+    console.log(attempt)
+    if (attempt > 4) {
+        wrong = wrong + 1;
+        break;
+    }
+    var GuisQ = prompt("What is my Age?!")
+    var X = GuisQ - 23
+    if (GuisQ - 23 == 0) {
+        alert("That is right");
+        right = right + 1;
+    } else if (GuisQ - 23 == 1) {
+        alert("you a little bet higher");
+    } else if (GuisQ - 23 == -1) {
+        alert("you a little bet lower");
+    } else if (GuisQ - 23 > 1) {
+        alert("No that is high");
+    } else if (GuisQ - 23 < -1) {
+        alert("No that is low");
+    } else {
+        alert("NO");
+    }
+    // switch(X){
+    //     case '0':
+    //         alert("That is right");
+    //         right = right + 1 ;
+    //     case '1':
+    //         alert("you a little bet higher");
+    //     case '-1':
+    //         alert("you a little bet lower");
+    //     case (X > 1):
+    //         alert("No it is that is high");
+    //     case (X < 1):
+    //         alert("No it is that is low");
+    //     default:
+    //         alert("NO");
+    // }
+} while (GuisQ != '23');
+
+/*************************************************************** */
+var food = [
+    'Shaowerma',
+    'mansaf',
+    'mulukhia',
+    'oives',
+    'lazanui'
+];
+console.log(`right is ${right}`);
+console.log(`wrong is ${wrong}`);
+var done = false
+for (var index = 0; index < 6; index++) {
+    if (done == false) {
+        var FoodQ = prompt("What is my favorit food ?");
+        for (var i = 0; i < food.length; i++) {
+            if (FoodQ.toLowerCase() == food[i]) {
+                alert(`${FoodQ} is right`);
+                right = right + 1;
+                done = true;
+
+            }
+        }
+    }
+}
+if (done == false) {
+    wrong = wrong + 1;
+}
 
 document.getElementById('UserName').textContent = `Thank you for visiting my web ${UserName}`
 document.getElementById("result").textContent = `${right} of your answer was right and ${wrong} was worng`
